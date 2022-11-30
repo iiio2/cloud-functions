c0bf1767aa391699ca5d0cab6ae5c95411a43b53
Mr. Robot <gomexxxx71@gmail.com>
2022-11-30 11:39:22 +0600

    added post req.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -12,7 +14,7 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   const { x, y } = req.body;
   const result = x + y;
-  res.send(result);
+  res.status(200).send(result.toString());
 });
 
 exports.app = functions.https.onRequest(app);
